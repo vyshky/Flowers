59c545d1c419f4dabce5f64c329ab44835b36209
Александр Дедюхин <vyshky2@Gmail.com>
2021-10-21 10:49:21 +0400

    Update Experience.java

--- src/main/java/org/nextDay/Experience.java
+++ src/main/java/org/nextDay/Experience.java
@@ -35,45 +35,6 @@ public class Experience {
         experience -= experienceNextLevel;
     }
 
-
-//    private void initLevelUpDay(int daysToBloom) {
-//        var levelUp = daysToBloom / height.count();
-//        if (levelUp <= 1) levelUp = 2;
-//
-//        experience = levelUp;
-//    }
-
-    //Округляем дату до дня цветения, что бы не выводить лишние дни
-//    private int roundUpTheDate(int daysToBloom) {
-//
-//        var experienceDay = (daysToBloom / height.count()); // получаем количество дней после которого растение будет вырастать на один уровень
-//
-//        var countExperienceDays = daysToBloom / experienceDay;  // считаем количество повышения уровня за daysToBloom дней
-//
-//        var dayMaxLevel = experienceDay * countExperienceDays;  // записываем последний день в который будет повышение уровня
-//
-//        return dayMaxLevel;
-//    }
-
-//    public void nextDay() {
-//        ++landingDay;
-//        if (landingDay == dayLevelUp) {
-//            System.out.print(flowerName + " " + height + ", ");
-//        } else if (landingDay % dayLevelUp == 0 && landingDay != 0) {
-//            height = height.nextIndex();
-//            System.out.print(flowerName + " " + height + ", ");
-//        }
-//        hungerLevel += 10;
-//    }
-
-
-//    @Override
-//    public String toString() {
-//        return "experience{" +
-//                "height=" + height +
-//                ", daysFromLanding=" + landingDay +
-//                '}';
-//    }
 }
 
 
